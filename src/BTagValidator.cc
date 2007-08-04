4e1f55ab0119c12a141d7c37c05d383a464dfc24
cmsbuild <>
2007-08-04 09:00:35 +0000

    This commit was manufactured by cvs2git to create tag 'Validation- RecoB-V00-00-06'.
    Sprout from master 2007-04-12 17:18:42 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Use new plug-in manager'
    Cherrypick from master 2007-08-04 09:00:34 UTC Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch> 'Fix include (temporarily removes on change which was sitting at HEAD,':
        src/BTagValidator.cc

--- src/BTagValidator.cc
+++ src/BTagValidator.cc
@@ -6,7 +6,7 @@
  author: Victor Bazterra, UIC
          Francisco Yumiceva, Fermilab (yumiceva@fnal.gov)
 
- version $Id: BTagValidator.cc,v 1.6 2007/02/20 02:00:46 yumiceva Exp $
+ version $Id: BTagValidator.cc,v 1.9 2007/06/24 20:13:45 yumiceva Exp $
 
 ________________________________________________________________**/
 
@@ -22,7 +22,7 @@ ________________________________________________________________**/
 
 #include "RecoBTag/Analysis/interface/BaseBTagPlotter.h"
 
-#include "DataFormats/BTauReco/interface/TrackCountingTagInfoFwd.h"
+#include "DataFormats/BTauReco/interface/TrackCountingTagInfo.h"
 
 #include "FWCore/Utilities/interface/Exception.h"
 
@@ -41,6 +41,10 @@ BTagValidator::BTagValidator(const edm::ParameterSet& iConfig) {
 	algorithm_ = iConfig.getParameter<std::string>( "algorithm" );
 	rootFile_ = iConfig.getParameter<std::string>( "rootfile" );
 	DQMFile_ = iConfig.getParameter<std::string>( "DQMFile" );
+//	TString tversion(edm::getReleaseVersion());
+//	tversion = tversion.Remove(0,1);
+//	tversion = tversion.Remove(tversion.Length()-1,tversion.Length());
+//	DQMFile_  = std::string(tversion)+"_"+DQMFile_;
 	histogramList_ = iConfig.getParameter<vstring>( "histogramList" );
 	referenceFilename_ = iConfig.getParameter<std::string>( "referenceFilename" );
 	doCompare_ = iConfig.getParameter<bool>( "compareHistograms");
@@ -48,11 +52,11 @@ BTagValidator::BTagValidator(const edm::ParameterSet& iConfig) {
 					
 	if (doAnalysis_) {
 		if (algorithm_ == "TrackCounting") 
-			petBase_ = new BTagPABase<reco::TrackCountingTagInfoCollection, TrackCountingTagPlotter>(iConfig);
+			petBase_ = new BTagPABase< TrackCountingTagPlotter>(iConfig);
 		else if (algorithm_ == "TrackProbability")
-			petBase_ = new BTagPABase<reco::TrackProbabilityTagInfoCollection, TrackProbabilityTagPlotter>(iConfig);
+			petBase_ = new BTagPABase< TrackProbabilityTagPlotter>(iConfig);
 		else if (algorithm_ == "SoftLepton")
-			petBase_ = new BTagPABase<reco::SoftLeptonTagInfoCollection, SoftLeptonTagPlotter>(iConfig);
+			petBase_ = new BTagPABase< SoftLeptonTagPlotter>(iConfig);
 		else 
 			throw cms::Exception("UnknownAlgorithm") << algorithm_ << " possible options are TrackCounting, TrackProbability and SoftLepton. ";
 	}
